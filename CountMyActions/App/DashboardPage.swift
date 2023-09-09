759c9dbe81e693ccfe826877a181b254d03ff687
prashantvaid <92187498+prashantvaid@users.noreply.github.com>
2023-09-08 23:05:36 -0700

    dddd

--- CountMyActions/App/DashboardPage.swift
+++ CountMyActions/App/DashboardPage.swift
@@ -1,16 +1,114 @@
-//
-//  DashboardPage.swift
-//  CountMyActions
-//
-//  Created by pv on 9/8/23.
-//  Copyright © 2023 Apple. All rights reserved.
-//
-
 import SwiftUI
 
 struct DashboardPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView {
+            TabView {
+            ZStack {
+                VStack {
+                    Image("background")
+                        .resizable()
+                        .scaledToFill()
+                        .edgesIgnoringSafeArea(.all)
+                        .opacity(0.5)
+                        .frame(height: 250)
+                    
+                    Spacer()
+                }
+                
+                VStack(alignment: .leading) {
+                    VStack(alignment: .leading) {
+                        Text("Welcome User!")
+                            .font(.title)
+                        
+                        Text("Courses Completed: ")
+                            .font(.headline)
+                        
+                        Text("0")
+                            .font(.title)
+                        
+                        Text("Total letters: ")
+                            .font(.headline)
+                        
+                        Text("0")
+                            .font(.title)
+                        
+                        Text("Total Words")
+                            .font(.headline)
+                        
+                        Text("0")
+                            .font(.title)
+                    }
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    Spacer()
+                    
+                    // Add 2x2 Card Grid
+                    LazyVGrid(columns: Array(repeating: GridItem(), count: 2), spacing: 10) {
+                        ForEach(0..<4) { index in
+                            CardView(index: index)
+                        }
+                    }
+                    .padding()
+                    
+                    Spacer()
+                }
+                .padding()
+            }
+            .navigationBarBackButtonHidden(true)
+        }
+        .toolbar {
+            ToolbarItem(placement: .bottomBar) {
+                HStack {
+                    
+                    Button(action: {
+                        // Action for Home button
+                    }) {
+                        Image(systemName: "house")
+                            .font(.title)
+                            .foregroundColor(.blue)
+                    }
+                    .padding()
+                    
+                    Button(action: {
+                        // Action for Hand button
+                    }) {
+                        Image(systemName: "hand.raised")
+                            .font(.title)
+                            .foregroundColor(.blue)
+                    }
+                    .padding()
+                    
+                    Button(action: {
+                        // Action for Checkmark button
+                    }) {
+                        Image(systemName: "checkmark")
+                            .font(.title)
+                            .foregroundColor(.blue)
+                    }
+                    .padding()
+                    
+                }
+            }
+        }
+    }
+}
+}
+
+struct CardView: View {
+    var index: Int
+    
+    var body: some View {
+        let cardTitles = ["Letters", "Phrases", "Dictionary", "Games"]
+        
+        RoundedRectangle(cornerRadius: 10)
+            .fill(Color.blue)
+            .frame(height: 100)
+            .overlay(
+                Text(cardTitles[index])
+                    .foregroundColor(.white)
+                    .font(.headline)
+            )
     }
 }
 
--- CountMyActions/App/ListView.swift
+++ CountMyActions/App/ListView.swift
@@ -1,16 +1,8 @@
-//
-//  ListView.swift
-//  CountMyActions
-//
-//  Created by Yash Seth on 9/9/23.
-//  Copyright © 2023 Apple. All rights reserved.
-//
-
 import SwiftUI
 
 struct ListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello World!")
     }
 }
 
@@ -19,3 +11,4 @@ struct ListView_Previews: PreviewProvider {
         ListView()
     }
 }
+
