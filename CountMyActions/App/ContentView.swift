eed5a9d30b4a70e6a2cc6ebde49527170a81a5f9
prashantvaid <92187498+prashantvaid@users.noreply.github.com>
2023-09-09 02:55:45 -0700

    jjjjjj

--- CountMyActions/App/ContentView.swift
+++ CountMyActions/App/ContentView.swift
@@ -4,7 +4,6 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack {
-                // This Color view will fill the entire screen
                 Color("LightBackground").edgesIgnoringSafeArea(.all)
 
                 VStack {
