37ac69be604401b77ee3c446332f5c0861857c8b
prashantvaid <92187498+prashantvaid@users.noreply.github.com>
2023-09-08 21:34:34 -0700

    jjjjj

---
+++ CountMyActions/App/DashboardPage.swift
@@ -0,0 +1,21 @@
+//
+//  DashboardPage.swift
+//  CountMyActions
+//
+//  Created by pv on 9/8/23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct DashboardPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DashboardPage_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardPage()
+    }
+}
--- CountMyActions/App/HomePage2.swift
+++ CountMyActions/App/HomePage2.swift
@@ -1,21 +1,41 @@
-//
-//  HomePage2.swift
-//  CountMyActions
-//
-//  Created by pv on 9/8/23.
-//  Copyright © 2023 Apple. All rights reserved.
-//
-
 import SwiftUI
 
 struct HomePage2: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text("Save Data")
+                
+                Image("save")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                
+                Spacer()
+                
+                NavigationLink(destination: DashboardPage()) {
+                    Circle()
+                        .frame(width: 60, height: 60)
+                        .foregroundColor(.red)
+                        .overlay(
+                            Image(systemName: "arrow.right")
+                                .foregroundColor(.white)
+                        )
+                        .shadow(radius: 4)
+                        .padding(.top, 20)
+                }
+                
+                Spacer()
+            }
+        }
+        .navigationBarBackButtonHidden(true)
     }
 }
+
 
 struct HomePage2_Previews: PreviewProvider {
     static var previews: some View {
         HomePage2()
     }
 }
+
