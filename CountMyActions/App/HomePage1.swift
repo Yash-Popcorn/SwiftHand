7523d471eacbafe05a3deb30a137ddcfd6391979
Yash S <yashseth.vik@gmail.com>
2023-09-09 00:33:57 -0400

    jjjjj

--- CountMyActions/App/HomePage1.swift
+++ CountMyActions/App/HomePage1.swift
@@ -3,32 +3,38 @@ import SwiftUI
 struct HomePage1: View {
     var body: some View {
         NavigationView {
-            VStack {
-                HStack {
-                    Text("Free learning with")
-                    Text("Hands-On")
-                        .foregroundColor(.blue)
-                    Text("Experience")
-                }
-
-                Image("hand")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 200, height: 200)
+            ZStack {
+                Color("LightBackground").edgesIgnoringSafeArea(.all)
                 
-                NavigationLink(destination: HomePage2()) {
-                    Circle()
-                        .frame(width: 60, height: 60)
-                        .foregroundColor(.red)
-                        .overlay(
-                            Image(systemName: "arrow.right")
-                                .foregroundColor(.white)
-                        )
-                        .shadow(radius: 4)
+                VStack {
+                    HStack {
+                        Text("Free learning with") +
+                        Text(" Hands-On").foregroundColor(.blue) +
+                        Text(" Experience")
+                    }
+                    .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    
+                    Image("hand")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 600, height: 500)
+                    
+                    NavigationLink(destination: HomePage2()) {
+                        Circle()
+                            .frame(width: 120, height: 120)
+                            .foregroundColor(.red)
+                            .overlay(
+                                Image(systemName: "arrow.right")
+                                    .foregroundColor(.white)
+                            )
+                            .shadow(radius: 4)
+                    }
+                    .padding(.top, 20)
+                    
+                    Spacer()
                 }
-                .padding(.top, 20)
-                
-                Spacer()
             }
         }
         .navigationBarBackButtonHidden(true)
