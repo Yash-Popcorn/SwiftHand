75bb2332c84ed54597f84092f02c96842e8a84ae
Yash S <yashseth.vik@gmail.com>
2023-09-09 00:14:53 -0400

    jjjjjjjjjjj

--- CountMyActions/App/ContentView.swift
+++ CountMyActions/App/ContentView.swift
@@ -16,25 +16,35 @@ struct ContentView: View {
                         .frame(width: 200, height: 200)
                     Spacer()
 
-                    VStack(alignment: .leading) {
-                        Text("Hands-On")
-                            .font(.system(size: 55))
-                            .fontWeight(.bold)
-                        Text(desc)
-                            .font(.body)
-                            .fontWeight(.semibold)
-                    }
+                    VStack(spacing: 25) {
+                        VStack(alignment: .leading) {
+                            Text("Hands-On")
+                                .font(.system(size: 55))
+                                .fontWeight(.bold)
+                            Text(desc)
+                                .font(.body)
+                                .fontWeight(.semibold)
+                        }
+                        VStack(alignment: .center, spacing: -10) {
+                            NavigationLink(destination: HomePage1()) {
+                                Text("Get Started")
+                                    .frame(width: 200, height: 50)
+                                    .background(Color.red)
+                                    .foregroundColor(.white)
+                                    .font(.headline)
+                                    .cornerRadius(10)
+                            }
+                            .padding(.bottom, 20)
+                            
+                            Text("Saves in icloud")
+                                .fontWeight(.semibold)
+                                .foregroundColor(Color.blue)
+
+                        }                    }
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                     Spacer()
 
-                    NavigationLink(destination: HomePage1()) {
-                        Text("Get Started")
-                            .frame(width: 200, height: 50)
-                            .background(Color.red)
-                            .foregroundColor(.white)
-                            .font(.headline)
-                            .cornerRadius(10)
-                    }
-                    .padding(.bottom, 20)
+                    
                 }
             }
         }
---
+++ CountMyActions/App/ListView.swift
@@ -0,0 +1,21 @@
+//
+//  ListView.swift
+//  CountMyActions
+//
+//  Created by Yash Seth on 9/9/23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
