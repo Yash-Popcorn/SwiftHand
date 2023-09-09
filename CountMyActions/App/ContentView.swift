b094f3e1d5248cfbc03ab4967b87fc18d9598cd3
Yash S <yashseth.vik@gmail.com>
2023-09-08 23:56:45 -0400

    jjjjjj

--- CountMyActions/App/ContentView.swift
+++ CountMyActions/App/ContentView.swift
@@ -1,37 +1,42 @@
 import SwiftUI
-
 struct ContentView: View {
+    var desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
     var body: some View {
         NavigationView {
-            VStack {
-                Spacer()
-                
-                Image("hearthand")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 200, height: 200)
-                Spacer()
-                
+            ZStack {
+                // This Color view will fill the entire screen
+                Color("LightBackground").edgesIgnoringSafeArea(.all)
+
                 VStack {
-                    Text("Hands-On")
-                        .font(.system(size: 55))
-                        .fontWeight(.bold)
-                    
+                    Spacer()
+
+                    Image("hearthand")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200, height: 200)
+                    Spacer()
+
+                    VStack(alignment: .leading) {
+                        Text("Hands-On")
+                            .font(.system(size: 55))
+                            .fontWeight(.bold)
+                        Text(desc)
+                            .font(.body)
+                            .fontWeight(.semibold)
+                    }
+                    Spacer()
+
+                    NavigationLink(destination: HomePage1()) {
+                        Text("Get Started")
+                            .frame(width: 200, height: 50)
+                            .background(Color.red)
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .cornerRadius(10)
+                    }
+                    .padding(.bottom, 20)
                 }
-                Spacer()
-                
-                NavigationLink(destination: HomePage1()) {
-                    Text("Get Started")
-                        .frame(width: 200, height: 50)
-                        .background(Color.red)
-                        .foregroundColor(.white)
-                        .font(.headline)
-                        .cornerRadius(10)
-                }
-                .padding(.bottom, 20)
             }
-            .background(Color.blue)
-            .edgesIgnoringSafeArea(.all)
         }
     }
 }
